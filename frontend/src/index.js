99c039beae0c075dc48de05f77cd3d4a1a120315
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-11-21 22:41:48 +0530

    Integrated Redux and Created Slices for Notes and Users, Also Created Component for NotesList, UsersList, Note and User

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {store} from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path='/*' element={<App/>}/>
       </Routes>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
