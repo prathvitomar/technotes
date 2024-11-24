624bc16fe4606a5a3fbe0e3cfa1495021549355e
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-11-24 23:34:03 +0530

    Integrated Authentication using JWT Token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,25 +6,33 @@ import DashLayout from "./components/DashLayout";
 import Login from "./features/auth/Login";
 import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
 import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Public />} />
-          <Route path="login" element={<Login />} />
-          <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome />} />
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-            </Route>
-            <Route path="users">
-              <Route index element={<UsersList />} />
-            </Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="users">
+            <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUserForm />} />
+          </Route>
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditNote />} />
+            <Route path="new" element={<NewNote />} />
           </Route>
         </Route>
-      </Routes>
+      </Route>
+    </Routes>
   );
 }
 
