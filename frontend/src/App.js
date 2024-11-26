6a8a1f2812372dc61465655ce359dfca5030e763
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-11-26 23:23:05 +0530

    Added Functionality to persist login state on refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
-import Public from "./components/Public";
-import DashLayout from "./components/DashLayout";
-import Login from "./features/auth/Login";
-import Welcome from "./features/auth/Welcome";
-import NotesList from "./features/notes/NotesList";
-import EditNote from "./features/notes/EditNote";
-import NewNote from "./features/notes/NewNote";
-import UsersList from "./features/users/UsersList";
-import EditUser from "./features/users/EditUser";
-import NewUserForm from "./features/users/NewUserForm";
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/auth/Login';
+import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
+import EditUser from './features/users/EditUser'
+import NewUserForm from './features/users/NewUserForm'
+import EditNote from './features/notes/EditNote'
+import NewNote from './features/notes/NewNote'
+import Prefetch from './features/auth/Prefetch'
+import PersistLogin from './features/auth/PersistLogin';
 
 function App() {
   return (
@@ -18,19 +19,29 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome />} />
-          <Route path="users">
-            <Route index element={<UsersList />} />
-            <Route path=":id" element={<EditUser />} />
-            <Route path="new" element={<NewUserForm />} />
-          </Route>
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-            <Route path=":id" element={<EditNote />} />
-            <Route path="new" element={<NewNote />} />
+
+        <Route element={<PersistLogin />}>
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
+
+              <Route index element={<Welcome />} />
+
+              <Route path="users">
+                <Route index element={<UsersList />} />
+                <Route path=":id" element={<EditUser />} />
+                <Route path="new" element={<NewUserForm />} />
+              </Route>
+
+              <Route path="notes">
+                <Route index element={<NotesList />} />
+                <Route path=":id" element={<EditNote />} />
+                <Route path="new" element={<NewNote />} />
+              </Route>
+
+            </Route>{/* End Dash */}
           </Route>
         </Route>
+
       </Route>
     </Routes>
   );
